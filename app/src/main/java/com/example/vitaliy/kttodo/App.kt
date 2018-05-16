60a6bed6920648419eed3c7c7dce5a59ab673270
Vitaliy Balash <vitalik.balash@gmail.com>
2018-05-16 11:49:57 +0300

    add LeakCanary

--- app/src/main/java/com/example/vitaliy/kttodo/App.kt
+++ app/src/main/java/com/example/vitaliy/kttodo/App.kt
@@ -2,6 +2,7 @@ package com.example.vitaliy.kttodo
 
 import android.app.Application
 import com.example.vitaliy.kttodo.reducers.appReducer
+import com.squareup.leakcanary.LeakCanary
 import tw.geothings.rekotlin.Store
 
 val mainStore = Store(state = null, reducer = ::appReducer)
@@ -9,6 +10,13 @@ val mainStore = Store(state = null, reducer = ::appReducer)
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this);
+
         println("HI")
     }
 }
